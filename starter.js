66e2d852a66d9ba54afd9474a051b412798314bf
Purushottam Sharma <purusharma128@gmail.com>
2021-12-07 12:03:48 +0000

    Added an add() function

---
+++ starter.js
@@ -0,0 +1,13 @@
+function looper() {
+  return Array(5)
+    .keys()
+    .map((_i) => ".");
+}
+
+console.log(looper());
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(add(5, 10));
